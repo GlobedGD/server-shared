c4720c07cd0ff0f4a2fe89902a1dd27795d03651
dankmeme01 <42031238+dankmeme01@users.noreply.github.com>
2025-08-03 20:51:21 +0200

    fix logger to use non blocking stdout

--- src/logging.rs
+++ src/logging.rs
@@ -1,10 +1,10 @@
 use std::path::Path;
-use tracing::level_filters::LevelFilter;
+use tracing::{Level, level_filters::LevelFilter};
 use tracing_appender::{
     non_blocking::{NonBlockingBuilder, WorkerGuard},
     rolling,
 };
-use tracing_subscriber::{Layer as _, Registry, fmt::Layer, layer::SubscriberExt};
+use tracing_subscriber::{Registry, filter, fmt::Layer, layer::SubscriberExt};
 
 pub fn setup_logger(
     rolling: bool,
@@ -12,20 +12,9 @@ pub fn setup_logger(
     file_name_prefix: impl AsRef<Path>,
     level: &str,
     file_enabled: bool,
-) -> WorkerGuard {
-    let appender = if rolling {
-        rolling::daily(directory, file_name_prefix)
-    } else {
-        rolling::never(directory, file_name_prefix)
-    };
-
-    let (nb, guard) = NonBlockingBuilder::default()
-        .lossy(true)
-        .thread_name("Log writer thread")
-        .buffered_lines_limit(8192)
-        .finish(appender);
-
-    let log_level = match level {
+) -> (WorkerGuard, WorkerGuard) {
+    // Setup log level filter
+    let log_level = match level.to_lowercase().as_str() {
         "error" => LevelFilter::ERROR,
         "warn" => LevelFilter::WARN,
         "info" => LevelFilter::INFO,
@@ -34,24 +23,43 @@ pub fn setup_logger(
         _ => LevelFilter::INFO,
     };
 
-    let stdout_layer = Layer::default()
-        .with_writer(std::io::stdout)
-        .with_filter(log_level);
+    let filter = filter::Targets::new()
+        .with_target("sqlx", Level::WARN)
+        .with_default(log_level);
 
-    let subscriber = Registry::default().with(stdout_layer);
+    // Create file appender
+    let appender = if rolling {
+        rolling::daily(directory, file_name_prefix)
+    } else {
+        rolling::never(directory, file_name_prefix)
+    };
+
+    let (nb_file, guard_file) = NonBlockingBuilder::default()
+        .lossy(true)
+        .thread_name("Log writer thread")
+        .buffered_lines_limit(8192)
+        .finish(appender);
+
+    // Create stdout layer
+    let (nb_stdout, guard_stdout) = NonBlockingBuilder::default()
+        .lossy(true)
+        .thread_name("Stdout log writer thread")
+        .buffered_lines_limit(8192)
+        .finish(std::io::stdout());
+
+    let stdout_layer = Layer::default().with_writer(nb_stdout);
+
+    let subscriber = Registry::default().with(filter).with(stdout_layer);
 
     if file_enabled {
-        let subscriber = subscriber.with(
-            Layer::default()
-                .with_writer(nb)
-                .with_ansi(false)
-                .with_filter(log_level),
-        );
+        let file_layer = Layer::default().with_writer(nb_file).with_ansi(false);
+        let subscriber = subscriber.with(file_layer);
+
         tracing::subscriber::set_global_default(subscriber)
     } else {
         tracing::subscriber::set_global_default(subscriber)
     }
     .expect("failed to set global subscriber");
 
-    guard
+    (guard_file, guard_stdout)
 }
